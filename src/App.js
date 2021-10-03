eed46a6794dc913b8f943ef09a84cb4184490d09
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-10-03 21:51:44 +0530

    Still working on it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState([]);
+
+  console.log(user);
+
+  function Getuser() {
+    fetch(`https://reqres.in/api/users?page=1`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setUser(data.data);
+      });
+  }
+
+  setTimeout(() => {}, 4000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <button onClick={Getuser}>Get Users</button>
+      </nav>
+
+      <main>
+        <div className="user-container">
+          {user.map((eachUser) => {
+            return (
+              <>
+                <div className="user-card">
+                  <img src={eachUser.avatar} alt="user_img" />
+                  <h2>
+                    {eachUser.first_name} {eachUser.last_name}
+                  </h2>
+                  <p>
+                    {" "}
+                    {eachUser.email} <i className="fas fa-envelope"></i>{" "}
+                  </p>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </main>
+    </>
   );
 }
 
